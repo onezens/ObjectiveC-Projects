8642bd7274416325aefd1c8903fa58fe72d861b1
wackosix <mail@wackosix.cn>
2016-06-12 16:38:53 +0800

    init project & add auto localization files

---
+++ WSWeibo/Core/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSWeibo
+//
+//  Created by wackosix on 16/6/12.
+//  Copyright © 2016年 www.wackosix.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSWeibo/Core/Main/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WSWeibo
+//
+//  Created by wackosix on 16/6/12.
+//  Copyright © 2016年 www.wackosix.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

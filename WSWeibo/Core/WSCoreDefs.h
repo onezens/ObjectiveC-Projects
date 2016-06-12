83f6cc21e6f49b89ec8b390c105b742d01a2892f
wackosix <mail@wackosix.cn>
2016-06-12 18:35:51 +0800

    fix some inssue

---
+++ WSWeibo/Core/WSCoreDefs.h
@@ -0,0 +1,17 @@
+//
+//  WSCoreDefs.h
+//  WSWeibo
+//
+//  Created by wackosix on 16/6/12.
+//  Copyright © 2016年 www.wackosix.cn. All rights reserved.
+//
+
+#ifdef WSWeibo_DEBUG
+
+#define kBaseURL @"WSWeibo_DEBUG"
+
+#else
+
+#define kBaseURL @"WSWeibo_RELEASE"
+
+#endif

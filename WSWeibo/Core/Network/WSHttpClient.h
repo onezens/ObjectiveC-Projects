83a6680a0831fe92d350ace0737d53eab1fcd903
wackosix <mail@wackosix.cn>
2016-06-14 14:11:14 +0800

    调整项目的机构

---
+++ WSWeibo/Core/Network/WSHttpClient.h
@@ -0,0 +1,15 @@
+//
+//  WSHttpClient.h
+//  WSWeibo
+//
+//  Created by wackosix on 16/6/14.
+//  Copyright © 2016年 www.wackosix.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSHttpClient : NSObject
+
+
+
+@end
---
+++ WSWeibo/Core/Network/WSHttpClient.m
@@ -0,0 +1,13 @@
+//
+//  WSHttpClient.m
+//  WSWeibo
+//
+//  Created by wackosix on 16/6/14.
+//  Copyright © 2016年 www.wackosix.cn. All rights reserved.
+//
+
+#import "WSHttpClient.h"
+
+@implementation WSHttpClient
+
+@end

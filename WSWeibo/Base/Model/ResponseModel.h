6ac272c6eb34f20304a50ba8d0f28bee681ac0c1
wackosix <mail@wackosix.cn>
2016-06-22 11:30:08 +0800

    划分模块，调整目录

--- WSWeibo/Base/Model/ResponseModel.h
+++ WSWeibo/Base/Model/ResponseModel.h
@@ -10,10 +10,10 @@
 
 @interface ResponseModel : NSObject
 
-@property (nonatomic, copy) NSString *reqName;
+@property (nonatomic, copy) NSString <Optional> *reqName;
+@property (nonatomic, strong) id <Optional> resObj;
+@property (nonatomic, copy) NSString <Optional> *resInfo;
 @property (nonatomic, assign) NSInteger status;
-@property (nonatomic, strong) id resObj;
-@property (nonatomic, copy) NSString *resInfo;
 
 + (instancetype)resSuccessModelWithName:(NSString *)reqName response:(id)resObj;
 + (instancetype)resFailedModelWithName:(NSString *)reqName status:(NSInteger)status errorInfo:(NSString *)info;
